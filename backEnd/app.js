27b7a0111e10e95af15a7005c895bb86cd5c9f26
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2022-08-28 22:04:18 +0200

    display pictures working

--- backEnd/app.js
+++ backEnd/app.js
@@ -22,11 +22,12 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
   next();
 });
 
 app.use('/api/auth', usersRoutes);
 app.use('/api/sauces', stuffRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/images',express.static(path.join(__dirname,'images')));
 app.use(helmet());
 module.exports = app;
