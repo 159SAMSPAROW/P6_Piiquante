42d7b79f7ea769017030e58ab2e625cc26c26374
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2022-08-30 20:30:19 +0200

    add app.use helmet CrossOriginResourcePolicy

--- backEnd/app.js
+++ backEnd/app.js
@@ -41,21 +41,22 @@ mongoose.connect('mongodb+srv://SpArOw:znrjDV35Nmx1qR8a@cluster0.6q837te.mongodb
 //Appel de la fonction middleware intégrée dans Express qui analyse les requêtes JSON entrantes et place les données analysées dans req.body.
 app.use(express.json());
 
+app.use(helmet({CrossOriginResourcePolicy: { policy: "same-site"}}));
 /////Middlware de définition des headers
 //Accéder à notre API depuis n'importe quelle origine ( '*' ) ;
 //Ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
 //Envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
 //Seules les demandes provenant du même site peuvent lire la ressource et  la protection contre certaines demandes d'autres origines
 app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
   next();
 });
 
 app.use('/api/auth', usersRoutes);
 app.use('/api/sauces', stuffRoutes);
 app.use('/images',express.static(path.join(__dirname,'images')));
-app.use(helmet());
+
 module.exports = app;
